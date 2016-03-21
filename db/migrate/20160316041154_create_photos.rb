0ee4337c9e94ddd9d147de17f5b271e7c344b3dc
Diane <diane@code12labs.com>
2016-03-20 23:48:30 -0500

    Migrate databases

--- db/migrate/20160316041154_create_photos.rb
+++ db/migrate/20160316041154_create_photos.rb
@@ -1,8 +1,15 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
+      t.text :caption
+      t.integer :place_id
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.string :picture
 
       t.timestamps
     end
+
+    add_index "photos", ["place_id"], name: "index_photos_on_place_id"
   end
 end
--- db/migrate/20160316041156_create_sayings.rb
+++ db/migrate/20160316041156_create_sayings.rb
@@ -1,6 +1,8 @@
 class CreateSayings < ActiveRecord::Migration
   def change
     create_table :sayings do |t|
+      t.string :saying
+      t.string :author
 
       t.timestamps
     end
